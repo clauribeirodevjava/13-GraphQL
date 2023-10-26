a1fdff012ab283fbcf03220622d4d76185ea72c0
Claudemir Ribeiro <clauribeirodevjava43@gmail.com>
2023-10-25 23:38:45 -0300

    schemas

---
+++ cmd/server/server.go
@@ -0,0 +1,48 @@
+package main
+
+import (
+	"database/sql"
+	"log"
+	"net/http"
+	"os"
+
+	"github.com/99designs/gqlgen/graphql/handler"
+	"github.com/99designs/gqlgen/graphql/playground"
+	"github.com/clauribeirodevjava/13-GraphQL.git/graph"
+	"github.com/clauribeirodevjava/13-GraphQL.git/graph/internal/database"
+)
+
+const defaultPort = "8080"
+
+func main() {
+	username := "root"
+	password := "root"
+	host := "localhost:3306" // Endereço do servidor MySQL
+	databaseName := "mysql"
+
+	// Crie a string de conexão
+	connectionString := username + ":" + password + "@tcp(" + host + ")/" + databaseName
+
+	// Abra a conexão com o banco de dados
+	db, err := sql.Open("mysql", connectionString)
+	if err != nil {
+		panic(err.Error())
+	}
+	defer db.Close()
+	categoryDb := database.NewCategory(db)
+
+	port := os.Getenv("PORT")
+	if port == "" {
+		port = defaultPort
+	}
+
+	srv := handler.NewDefaultServer(graph.NewExecutableSchema(graph.Config{Resolvers: &graph.Resolver{
+		CategoryDB: categoryDb,
+	}}))
+
+	http.Handle("/", playground.Handler("GraphQL playground", "/query"))
+	http.Handle("/query", srv)
+
+	log.Printf("connect to http://localhost:%s/ for GraphQL playground", port)
+	log.Fatal(http.ListenAndServe(":"+port, nil))
+}
